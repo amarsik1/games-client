d1d4615ba3de5937e126275a3db644465517f601
Artem <32891609+amarsik1@users.noreply.github.com>
2022-06-24 13:18:19 +0300

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const way = 'http://amarsik-games-node.herokuapp.com:3012';
+const way = 'amarsik-games-node.herokuapp.com:3012';
 
 export default io(way, { transports: ['websocket'] });
